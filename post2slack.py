c1973e2365c92e22119edc7024e232d1bd8d76b3
hitoyozake <mikoto_1122@yahoo.co.jp>
2019-11-11 12:19:58 +0900

    mod

--- post2slack.py
+++ post2slack.py
@@ -3,6 +3,7 @@
 import requests
 import json
 import os
+import logging
 
 class SlackClient:
 
@@ -12,23 +13,41 @@ class SlackClient:
     def __init__(self, token=""):
         self.token=token
         pass
+
     """
     """
     def upload2slack(self, channelId):
         abspath=os.path.abspath("./tmpOutput.png")
         file = {"file":open(abspath, "rb")}
-        print(abspath)
+        logging.info("file detail: {0}".format(abspath))
         payload = {"token": self.token, "title":"test",\
             "filename":"tmpOutput.png",\
             "initial_comment": "test",\
             "text":"test upload", "channels":channelId }
 
         # data = request.body params = urlに接続
-        resp = requests.post("https://slack.com/api/files.upload", data=payload, files=file)
-
-        print(resp)
+        try:
+            resp = requests.post("https://slack.com/api/files.upload", data=payload, files=file)
+        except:
+            return False
 
         return  True
+
+    def get_channel_message(channelId):
+        
+        return True
+
+
+
+    def post_message(self, channelId, text, user):
+        payload = {
+            "token": self.token,
+            "channels": channelId,
+            "text": text,
+            "user": user
+        }
+        return True
+
 
 
 if __name__ == '__main__':
